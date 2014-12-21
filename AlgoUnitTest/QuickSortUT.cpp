6335155c0f8a77f0d03943d2a4bfbf0977df33bc
Alexey Eremin <a.eremin.msu@gmail.com>
2014-12-21 15:08:46 +0300

    "Naive" quicksort and unit tests for it

---
+++ AlgoUnitTest/QuickSortUT.cpp
@@ -0,0 +1,77 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include <vector>
+#include <numeric>
+
+#include "../algo/QuickSort.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using std::vector;
+
+namespace AlgoUnitTest
+{
+    TEST_CLASS(QuickSortUT)
+    {
+    private:
+        static const int cDataSetSize = 2000;
+
+    public:
+
+        TEST_METHOD(SortsEmptyVector)
+        {
+            vector<int> empty;
+            quickSort(begin(empty), end(empty));
+        }
+
+        TEST_METHOD(SortsOneEltVector)
+        {
+            const int elt = 27;
+            vector<int> data;
+            data.push_back(elt);
+            quickSort(begin(data), end(data));
+            Assert::AreEqual(elt, data.front());
+        }
+
+        TEST_METHOD(SortsReverseOrderedVector)
+        {
+            vector<int> data(cDataSetSize);
+            std::iota(data.rbegin(), data.rend(), 0);
+            quickSort(begin(data), end(data));
+            for (auto iter = begin(data); iter != end(data); ++iter)
+                Assert::AreEqual(iter - begin(data), *iter);
+        }
+
+        TEST_METHOD(SortsOrderedVector)
+        {
+            vector<int> data(cDataSetSize);
+            std::iota(data.begin(), data.end(), 0);
+            quickSort(begin(data), end(data));
+            for (auto iter = begin(data); iter != end(data); ++iter)
+                Assert::AreEqual(iter - begin(data), *iter);
+        }
+
+        TEST_METHOD(SortsSameEltVector)
+        {
+            const int testelt = 27;
+            vector<int> data(cDataSetSize, testelt);
+            quickSort(begin(data), end(data));
+            for (auto elt : data)
+                Assert::AreEqual(testelt, elt);
+
+        }
+
+        TEST_METHOD(SortsRandomVector)
+        {
+            vector<int> data(cDataSetSize);
+            srand(17);
+            for (auto& elt : data)
+                elt = rand();
+
+            quickSort(begin(data), end(data));
+
+            for (auto iter = begin(data) + 1; iter != end(data); ++iter)
+                Assert::IsTrue(*iter >= *(iter - 1));
+        }
+
+    };
+}
