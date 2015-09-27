d29f6d38a9b4e344e01d67ab24bd2b2d2a8837e3
Alexey Eremin <a.eremin.msu@gmail.com>
2015-09-27 23:12:39 +0300

    Added testifcompiles.cpp file to test fused solution on compilability before sending

---
+++ Launcher/testifcompiles.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <numeric>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cstdint>
+#include <functional>
+
+using namespace std;
