bc26a95550c5aa6adf13394874ce3258e754b266
Alexey Eremin <a.eremin.msu@gmail.com>
2015-12-23 16:56:52 +0100

    Participation in Round 336 Div 1. A+, B+, C+.

---
+++ Solvers/609/Solver609A.cpp
@@ -0,0 +1,51 @@
+#include "Solvers/pch.h"
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+class Solver609A
+{
+public:
+	void run();
+};
+
+void Solver609A::run()
+{
+	int n;
+	cin >> n;
+	vector<pair<int, int>> beacons(n);
+	for (auto& p : beacons) cin >> p.first >> p.second;
+	sort(begin(beacons), end(beacons));
+	vector<int> numAlive;
+	for (int i = 0; i < n; ++i)
+	{
+		auto left = beacons[i].first - beacons[i].second;
+		auto it = lower_bound(begin(beacons), end(beacons), make_pair( left, 0 ));
+		int res = 0;
+		if (it == begin(beacons))
+			res = 1;
+		else
+			res = numAlive[it - begin(beacons) - 1] + 1;
+		numAlive.push_back(res);
+	}
+
+	auto maxIt = max_element(begin(numAlive), end(numAlive));
+	cout << (n - *maxIt);
+}
+
+class Solver609ATest : public ProblemTest {};
+
+TEST_F(Solver609ATest, DISABLED_Example1)
+{
+	setInput("4		1 9		3 1		6 1		7 4");
+	Solver609A().run();
+	EXPECT_EQ("1", getOutput());
+}
+
+TEST_F(Solver609ATest, Example2)
+{
+	setInput("7		1 1		2 1		3 1		4 1		5 1		6 1		7 1");
+	Solver609A().run();
+	EXPECT_EQ("3", getOutput());
+}
---
+++ Solvers/609/Solver609C.cpp
@@ -0,0 +1,71 @@
+#include "Solvers/pch.h"
+
+using namespace std;
+
+class Solver609C
+{
+public:
+	void run();
+};
+
+vector<int> calcPrefixFunction(const string& s)
+{
+	vector<int> pi;
+	if (s.empty())
+		return pi;
+	pi.push_back(0);
+	for (int i = 1; i < s.length(); ++i)
+	{
+		int curPi = pi.back();
+		while (curPi > 0 && s[i] != s[curPi])
+			curPi = pi[curPi - 1];
+		if (s[i] == s[curPi]) curPi++;
+		pi.push_back(curPi);
+	}
+	return pi;
+}
+
+void Solver609C::run()
+{
+	int n;
+	cin >> n;
+	std::string first, second;
+	cin >> first >> second;
+	std::set<char> lasts = { first.back(), second.back() };
+	
+	std::map<char, char> invertor = { {'N', 'S'}, {'S', 'N'}, {'W', 'E'}, {'E', 'W'} };
+	std::string conc;
+	for (auto i = n - 2; i >= 0; --i)
+		conc.push_back(invertor[first[i]]);
+
+	conc = conc + second;
+
+	auto pi = calcPrefixFunction(conc);
+	if (pi.back() > 0)
+		cout << "NO";
+	else
+		cout << "YES";
+}
+
+class Solver609CTest : public ProblemTest {};
+
+TEST_F(Solver609CTest, Example1)
+{
+	setInput("7		NNESWW		SWSWSW");
+	Solver609C().run();
+	EXPECT_EQ("YES", getOutput());
+}
+
+TEST_F(Solver609CTest, Example2)
+{
+	setInput("3		NN		SS");
+	Solver609C().run();
+	EXPECT_EQ("NO", getOutput());
+}
+
+TEST_F(Solver609CTest, Example3)
+{
+	setInput("3		NW		ES");
+	Solver609C().run();
+	EXPECT_EQ("NO", getOutput());
+}
