351dcb8c88ec8dbcc75c25a9ff93bd30bfc98999
Alexey Eremin <a.eremin.msu@gmail.com>
2015-12-23 16:52:40 +0100

    Virtual participation in Round 327 Div 1. A+, B+, C+.

--- Solvers/590/Solver590A.cpp
+++ Solvers/590/Solver590A.cpp
@@ -1,5 +1,7 @@
 #include "Solvers/pch.h"
 
+using namespace std;
+
 class Solver590A
 {
 public:
@@ -8,20 +10,73 @@ public:
 
 void Solver590A::run()
 {
+	int n;
+	cin >> n;
+	vector<int> nums(n + 2);
+	for (int i = 1; i <= n; ++i)
+		cin >> nums[i];
+	nums[0] = nums[1];
+	nums[n + 1] = nums[n];
+
+	int lastStableEnd = 1;
+	int lastStableVal = nums[1];
+	int prev = lastStableVal;
+	vector<int> result = {};
+	int nIterations = 0;
+	for (int i = 2; i <= n + 1; ++i)
+	{
+		auto curr = nums[i];
+		if (curr != prev)
+		{
+			prev = curr;
+			continue;
+		}
+		auto currStableStart = i - 1;
+		auto currStableVal = curr;
+		auto nChangingBetween = (currStableStart - lastStableEnd - 1);
+		if (nChangingBetween % 2 == 0)
+		{
+			nIterations = max(nIterations, nChangingBetween / 2);
+			for (int j = lastStableEnd ; j < i; ++j) result.push_back( (j - lastStableEnd) <= nChangingBetween / 2 ? lastStableVal : currStableVal);
+		}
+		else
+		{
+			nIterations = max(nIterations, (nChangingBetween + 1) / 2);
+			for (int j = lastStableEnd ; j < i; ++j) result.push_back(currStableVal);
+		}
+		lastStableEnd = i;
+		lastStableVal = currStableVal;
+	}
+	cout << nIterations << endl;
+	for (auto& r : result) cout << r << " ";
 }
 
 class Solver590ATest : public ProblemTest {};
 
 TEST_F(Solver590ATest, Example1)
 {
-    setInput("5    4 1 2 5 3");
+	setInput("4		0 0 1 1");
 	Solver590A().run();
-    EXPECT_EQ("2", getOutput());
+	EXPECT_EQ("0\n0 0 1 1 ", getOutput());
 }
 
 TEST_F(Solver590ATest, Example2)
 {
-    setInput("4        4 1 3 2");
+	setInput("5		0 1 0 1 0");
 	Solver590A().run();
-    EXPECT_EQ("2", getOutput());
+	EXPECT_EQ("2\n0 0 0 0 0 ", getOutput());
+}
+
+TEST_F(Solver590ATest, Example3)
+{
+	setInput("7		0 0 1 0 1 0 0");
+	Solver590A().run();
+	EXPECT_EQ("2\n0 0 0 0 0 0 0 ", getOutput());
+}
+
+TEST_F(Solver590ATest, Example4)
+{
+	setInput("8		1 0 1 0 1 0 1 0");
+	Solver590A().run();
+	EXPECT_EQ("3\n1 1 1 1 0 0 0 0 ", getOutput());
 }
--- Solvers/590/Solver590B.cpp
+++ Solvers/590/Solver590B.cpp
@@ -8,20 +8,53 @@ public:
 
 void Solver590B::run()
 {
+	int64_t x1, y1, x2, y2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	x2 -= x1;
+	y2 -= y1;
+
+	int64_t vmax, t;
+	cin >> vmax >> t;
+
+	int64_t vx, vy, wx, wy;
+	cin >> vx >> vy >> wx >> wy;
+
+	double l = 0;
+	double r = 1e9;
+	while (r - l > 1e-10)
+	{
+		auto m = (r + l) / 2;
+		bool can = false;
+		if (m < t)
+		{
+			can = (x2 - m * vx) * (x2 - m * vx) + (y2 - m * vy) * (y2 - m * vy) < m * m * vmax * vmax;
+		}
+		else
+		{
+			auto tt = m - t;
+			can = (x2 - t * vx - tt * wx) * (x2 - t * vx - tt * wx) + (y2 - t * vy - tt * wy) * (y2 - t * vy - tt * wy) < m * m * vmax * vmax;
+		}
+		if (can)
+			r = m;
+		else
+			l = m;
+	}
+
+	cout << setprecision(9) << fixed << l;
 }
 
 class Solver590BTest : public ProblemTest {};
 
 TEST_F(Solver590BTest, Example1)
 {
-    setInput("5    4 1 2 5 3");
+	setInput("0 0 5 5		3 2		-1 -1		-1 0");
 	Solver590B().run();
-    EXPECT_EQ("2", getOutput());
+    EXPECT_EQ("3.729935587", getOutput());
 }
 
 TEST_F(Solver590BTest, Example2)
 {
-    setInput("4        4 1 3 2");
+	setInput("0 0 0 1000		100 1000		-50 0		50 0");
 	Solver590B().run();
-    EXPECT_EQ("2", getOutput());
+    EXPECT_EQ("11.547005384", getOutput());
 }
