56ab75eae2500454acaac1ec4bbfffd38fc05d2b
unknown <aeremin@seshat.adalisk.local>
2015-12-29 19:10:06 +0300

    Port 494A, 494B to new directory layout and test system

---
+++ Solvers/494/Solver494A.cpp
@@ -0,0 +1,87 @@
+#include <Solvers/pch.h>
+
+using namespace std;
+
+class Solver494A
+{
+public:
+    void run();
+};
+
+void Solver494A::run()
+{
+    string s;
+    cin >> s;
+    int numLeft = 0;
+    int numRight = 0;
+    int numSharp = 0;
+
+    vector<int> balance;
+    int lastSharp = 0;
+    for ( int i = 0; i < s.length(); ++i )
+    {
+        switch ( s[i] )
+        {
+        case '(': numLeft++; break;
+        case ')': numRight++; break;
+        case '#': numSharp++; lastSharp = i;  break;
+        }
+        balance.push_back( numLeft - numRight - numSharp );
+    }
+
+    if ( any_of( begin( balance ), end( balance ), []( int bal )
+    {
+        return bal < 0;
+    } ) )
+    {
+        cout << -1;
+        return;
+    }
+
+    for ( int i = lastSharp; i < balance.size(); ++i )
+    {
+        if ( balance[i] < balance.back() )
+        {
+            cout << -1;
+            return;
+        }
+    }
+
+    for ( int i = 0; i < numSharp - 1; ++i )
+    {
+        cout << 1 << '\n';
+    }
+    cout << 1 + balance.back();
+}
+
+class Solver494ATest : public ProblemTest
+{
+};
+
+TEST_F( Solver494ATest, Example1 )
+{
+    setInput("(((#)((#)");
+    Solver494A().run();
+    EXPECT_EQ("1\n2", getOutput());
+}
+
+TEST_F( Solver494ATest, Example2 )
+{
+    setInput("()((#((#(#()");
+    Solver494A().run();
+    EXPECT_EQ("1\n1\n3", getOutput());
+}
+
+TEST_F( Solver494ATest, Example3 )
+{
+    setInput( "#" );
+    Solver494A().run();
+    EXPECT_EQ( "-1", getOutput() );
+}
+
+TEST_F( Solver494ATest, Example4 )
+{
+    setInput( "(#)" );
+    Solver494A().run();
+    EXPECT_EQ( "-1", getOutput() );
+}
---
+++ Solvers/494/Solver494B.cpp
@@ -0,0 +1,72 @@
+#include <Solvers/pch.h>
+#include "algo/KnuttMorrisPratt.hpp"
+
+using namespace std;
+
+class Solver494B
+{
+public:
+    void run();
+};
+
+void Solver494B::run()
+{
+    const int MODULO = 1e9 + 7;
+
+    string s;
+    string t;
+    cin >> s;
+    cin >> t;
+
+    string concat = t + '$' + s;
+    auto pi = calc_prefix_function( concat );
+
+    int goodpi = t.length();
+
+    vector<int64_t> res;
+    vector<int64_t> sumres;
+
+    res.push_back( 1 );
+    sumres.push_back( 1 );
+    int lastStringStart = -1;
+
+    for ( int i = 0; i < s.length(); ++i )
+    {
+        if ( pi[i + goodpi + 1] == goodpi ) lastStringStart = i - goodpi + 1;
+        int64_t curres = res.back();
+        if ( lastStringStart >= 0 )
+        {
+            curres = ( curres + sumres[lastStringStart] ) % MODULO;
+        }
+        int64_t currsumres = ( sumres.back() + curres ) % MODULO;
+        res.push_back( curres );
+        sumres.push_back( currsumres );
+    }
+
+    cout << ( res.back() + MODULO - 1 ) % MODULO;
+}
+
+class Solver494BTest : public ProblemTest
+{
+};
+
+TEST_F( Solver494BTest, Example1 )
+{
+    setInput( "ababa              aba" );
+    Solver494B().run();
+    EXPECT_EQ( "5", getOutput() );
+}
+
+TEST_F( Solver494BTest, Example2 )
+{
+    setInput( "welcometoroundtwohundredandeightytwo              d" );
+    Solver494B().run();
+    EXPECT_EQ( "274201", getOutput() );
+}
+
+TEST_F( Solver494BTest, Example3 )
+{
+    setInput( "ddd              d" );
+    Solver494B().run();
+    EXPECT_EQ( "12", getOutput() );
+}
