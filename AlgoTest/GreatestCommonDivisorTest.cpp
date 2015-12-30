e1646712b59864e47110da900be34208c9ad4099
Alexey Eremin <a.eremin.msu@gmail.com>
2015-12-30 16:15:18 +0300

    Add function to represent greatest common divisor of two numbers as their linear combination

--- AlgoTest/GreatestCommonDivisorTest.cpp
+++ AlgoTest/GreatestCommonDivisorTest.cpp
@@ -15,3 +15,18 @@ TEST( GreatestCommonDivisorTest, ConcreteExamples )
 
     EXPECT_EQ( 2, greatestCommonDivisor( 104, 82 ) );
 }
+
+TEST( GreatestCommonDivisorTest, Decomposition )
+{
+    int64_t maxN = 100;
+    for ( int64_t p = -maxN; p <= maxN; ++p )
+    {
+        for ( int64_t q = -maxN; q <= maxN; ++q )
+        {
+            int64_t x, y;
+            auto gcd = gcdDecomposition( p, q, x, y );
+            EXPECT_EQ( gcd, p * x + q * y );
+            EXPECT_EQ( gcd, greatestCommonDivisor( p, q ) );
+        }
+    }
+}
--- algo/GreatestCommonDivisor.hpp
+++ algo/GreatestCommonDivisor.hpp
@@ -12,6 +12,25 @@ inline uint64_t gcdInternal( uint64_t a, uint64_t b )
     
     return gcdInternal( b, a % b );
 }
+
+inline uint64_t gcdDecompositionInternal( uint64_t a, uint64_t b, int64_t& x, int64_t& y )
+{
+    if ( b == 0 )
+    {
+        x = 1;
+        y = 0;
+        return a;
+    }
+
+    int64_t xx, yy;
+    auto gcd = gcdDecompositionInternal( b, a % b, xx, yy );
+    // ok, gcd = xx * b + (a % b) * yy = xx * b + (a - (a / b) * b) * yy =
+    //         = b * (xx - (a / b) * yy) + a * yy
+    x = yy;
+    y = xx - ( a / b ) * yy;
+    return gcd;
+}
+
 }
 
 inline uint64_t greatestCommonDivisor( int64_t a, int64_t b )
@@ -21,4 +40,14 @@ inline uint64_t greatestCommonDivisor( int64_t a, int64_t b )
     if ( a < b )
         std::swap( a, b );
     return gcdInternal( a, b );
+}
+
+inline uint64_t gcdDecomposition( int64_t a, int64_t b, int64_t& x, int64_t& y )
+{
+    auto aUnsigned = a * sgn( a );
+    auto bUnsigned = b * sgn( b );
+    auto gcd = ( aUnsigned < bUnsigned ) ? gcdDecompositionInternal( aUnsigned, bUnsigned, x, y ) : gcdDecompositionInternal( bUnsigned, aUnsigned, y, x );
+    x *= sgn( a );
+    y *= sgn( b );
+    return gcd;
 }
