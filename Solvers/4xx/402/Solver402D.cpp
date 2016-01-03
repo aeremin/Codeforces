52bd570b57fb703ce85b39f78cc715237c88a5b7
Alexey Eremin <a.eremin.msu@gmail.com>
2016-01-03 18:56:20 +0300

    402D+

---
+++ Solvers/4xx/402/Solver402D.cpp
@@ -0,0 +1,79 @@
+#include <Solvers/pch.h>
+#include "algo/GreatestCommonDivisor.hpp"
+#include "algo/Primes.hpp"
+
+using namespace std;
+
+class Solver402D
+{
+public:
+    void run();
+
+    unordered_set<int> badPrimes;
+
+    int f(int n)
+    {
+        auto primeDecomposition = decomposeToPrimePowers(n);
+        int res = 0;
+        for (auto p : primeDecomposition)
+            if (badPrimes.count(p.first))
+                res -= p.second;
+            else
+                res += p.second;
+        return res;
+    }
+};
+
+void Solver402D::run()
+{
+    int nNumbers, nBadPrimes;
+    cin >> nNumbers >> nBadPrimes;
+    vector<int> numbers(nNumbers);
+    for (auto& n : numbers)
+        cin >> n;
+
+    for (int i = 0; i < nBadPrimes; ++i)
+    {
+        int p;
+        cin >> p;
+        badPrimes.insert(p);
+    }
+
+    vector<int> gcds = { numbers[0] };
+    for (int i = 1; i < nNumbers; ++i)
+        gcds.push_back(greatestCommonDivisor(gcds.back(), numbers[i]));
+
+    vector<int> fgcds = { f(numbers[0]) };
+    for (int i = 1; i < nNumbers; ++i)
+        fgcds.push_back(fgcds.back() - f(gcds[i - 1] / gcds[i]));
+
+    int ans = 0;
+    int substractedF = 0;
+    for (int i = nNumbers - 1; i >= 0; --i)
+    {
+        int currF = fgcds[i] - substractedF;
+        if (currF < 0)
+            substractedF += currF;
+        ans += f(numbers[i]) - substractedF;
+    }
+
+    cout << ans;
+}
+
+class Solver402DTest : public ProblemTest
+{
+};
+
+TEST_F( Solver402DTest, Example1 )
+{
+    setInput("5 2        4 20 34 10 10        2 5");
+    Solver402D().run();
+    EXPECT_EQ("-2", getOutput());
+}
+
+TEST_F( Solver402DTest, Example2 )
+{
+    setInput("4 5        2 4 8 16        3 5 7 11 17");
+    Solver402D().run();
+    EXPECT_EQ("10", getOutput());
+}
