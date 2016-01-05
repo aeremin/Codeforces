246a549ecb264bab1a77880df85d13977abec7c8
Alexey Eremin <a.eremin.msu@gmail.com>
2016-01-06 00:08:01 +0300

    74B+

---
+++ Solvers/0xx/074/Solver074B.cpp
@@ -0,0 +1,67 @@
+#include <Solvers/pch.h>
+#include "algo/Sign.hpp"
+
+using namespace std;
+
+class Solver074B
+{
+public:
+    void run();
+};
+
+void Solver074B::run()
+{
+    int nCoaches, posStowaway, posController;
+    cin >> nCoaches >> posStowaway >> posController;
+    string s;
+    cin >> s >> s;
+    int dirController = (s == "tail") ? 1 : -1;
+    string schedule;
+    cin >> schedule;
+    for (int i = 0; i < schedule.length(); ++i)
+    {
+        if (schedule[i] == '0')
+        {
+            if (sgn(posStowaway - posController) == dirController)
+                posStowaway += dirController;
+            else
+                posStowaway -= dirController;
+            posStowaway = min(max(1, posStowaway), nCoaches);
+        }
+        else
+        {
+            if (dirController == 1)
+                posStowaway = 1;
+            else
+                posStowaway = nCoaches;
+        }
+        posController += dirController;
+        if (posController == 1 || posController == nCoaches)
+            dirController *= -1;
+
+        if (posController == posStowaway)
+        {
+            cout << "Controller " << (i + 1);
+            return;
+        }
+    }
+    cout << "Stowaway";
+}
+
+class Solver074BTest : public ProblemTest
+{
+};
+
+TEST_F(Solver074BTest, Example1)
+{
+    setInput("5 3 2\nto head\n0001001");
+    Solver074B().run();
+    EXPECT_EQ("Stowaway", getOutput());
+}
+
+TEST_F( Solver074BTest, Example2 )
+{
+    setInput("3 2 1\nto tail\n0001");
+    Solver074B().run();
+    EXPECT_EQ("Controller 2", getOutput());
+}
