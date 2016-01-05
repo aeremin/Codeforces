28828fe5d4dfc9d48ba0a302424ae423cba03da8
Alexey Eremin <a.eremin.msu@gmail.com>
2016-01-05 20:30:08 +0300

    Add Dijkstra algorithm implementation

---
+++ algo/Dijkstra.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+template<class PerEdgeData>
+struct DijkstraResult
+{
+    vector<PerEdgeData> minimalDistances;
+    vector<size_t> predecessors;
+};
+
+
+template<class PerEdgeData, class PerVertexData>
+DijkstraResult<PerEdgeData> getMinimalPathsFrom(const Graph<PerEdgeData, PerVertexData>& graph, size_t startVertex)
+{
+    DijkstraResult<PerEdgeData> result = { vector<PerEdgeData>(graph.vertexCount()), vector<size_t>(graph.vertexCount(), -1) };
+    using FullEdgeData = tuple<PerEdgeData, size_t, size_t>;
+    priority_queue<FullEdgeData, vector<FullEdgeData>, greater<FullEdgeData>> nextVerticesData;
+    nextVerticesData.push(make_tuple(PerEdgeData(), startVertex, startVertex));
+    while (!nextVerticesData.empty())
+    {
+        auto nextVertexData = nextVerticesData.top();
+        nextVerticesData.pop();
+        auto nextVertexInd = get<1>(nextVertexData);
+        if (result.predecessors[nextVertexInd] != -1)
+            continue;
+
+        auto nextVertexDist = get<0>(nextVertexData);
+        result.minimalDistances[nextVertexInd] = nextVertexDist;
+        result.predecessors[nextVertexInd] = get<2>(nextVertexData);
+        
+        for (auto& p : graph.vertexNeighbors(nextVertexInd))
+            nextVerticesData.push(make_tuple(nextVertexDist + p.second, p.first, nextVertexInd));
+    }
+
+    return result;
+}
