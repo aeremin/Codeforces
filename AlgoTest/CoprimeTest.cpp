c9d04bc0b878fb2acd8de355a1c9fc994f5757aa
Alexey Eremin <a.eremin.msu@gmail.com>
2016-01-09 19:46:12 +0300

    Add function to calculate set of coprimes with given number

---
+++ AlgoTest/CoprimeTest.cpp
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+#include "algo/numbertheory/Coprimes.hpp"
+#include "algo/numbertheory/Residue.hpp"
+#include "algo/QuickPower.hpp"
+
+TEST(CoprimeTest, CoprimeWithOne)
+{
+    auto divisors = getCoprimes(1);
+    EXPECT_EQ(std::vector<int64_t>({ 1 }), divisors);
+}
+
+TEST(CoprimeTest, CoprimeWithTwo)
+{
+    auto divisors = getCoprimes(2);
+    EXPECT_EQ(std::vector<int64_t>({ 1 }), divisors);
+}
+
+TEST(CoprimeTest, CoprimeWithPrime)
+{
+    auto divisors = getCoprimes(7);
+    EXPECT_EQ(std::vector<int64_t>({ 1, 2, 3, 4, 5, 6 }), divisors);
+}
+
+TEST(CoprimeTest, CoprimeWithNonPrime)
+{
+    auto divisors = getCoprimes(12);
+    EXPECT_EQ(std::vector<int64_t>({ 1, 5, 7, 11 }), divisors);
+}
+
+TEST(CoprimeTest, EulerTheorem)
+{
+    constexpr int64_t n = 123645;
+    auto coprimes = getCoprimes(n);
+    int64_t phiOfN = coprimes.size();
+    using ResidueModuloN = Residue<int64_t, n>;
+    for (auto a : coprimes)
+        EXPECT_EQ(ResidueModuloN(1), quickPower(ResidueModuloN(a), phiOfN));
+    
+}
---
+++ algo/numbertheory/Coprimes.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+#include "algo/numbertheory/Primes.hpp"
+
+//! Returns vector of numbers from [0, n], such that their GCD with n is 1.
+//! Order is guaranteed to be increasing.
+//! Time complexity is O(n log n) with some quite good constant.
+inline std::vector<int64_t> getCoprimes(int64_t n)
+{
+    std::vector<int64_t> res;
+    auto decompositionOfN = decomposeToPrimePowers(n);
+    for (int64_t i = 1; i <= n; ++i)
+    {
+        bool isCoprime = std::none_of(begin(decompositionOfN), end(decompositionOfN),
+            [&](const std::pair<uint64_t, uint64_t>& p) {return i % p.first == 0; });
+        if (isCoprime)
+            res.push_back(i);
+    }
+
+    return res;
+}
