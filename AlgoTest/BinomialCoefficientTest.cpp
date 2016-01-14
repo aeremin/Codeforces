1ce53cb94af9f687fff881bb7e76642a81c0a4f4
Alexey Eremin <a.eremin.msu@gmail.com>
2016-01-14 18:49:37 +0300

    Add function for binomial coefficients calculation and tests for it

---
+++ AlgoTest/BinomialCoefficientTest.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include "algo/algebra/BinomialCoefficients.hpp"
+
+TEST(BinomialCoefficientTest, ProperResultsForNEqual4)
+{
+	std::vector<int> expected = { 1, 4, 6, 4, 1 };
+	for (size_t i = 0; i < expected.size(); ++i)
+		EXPECT_EQ(expected[i], binomialCoefficient(1, 4, i)) << i;
+}
+
+TEST(BinomialCoefficientTest, SumOfBinomialCoefficientsIsPowerOfTwo)
+{
+	for (int64_t n = 0; n < 62; ++n)
+	{
+		int64_t sum = 0;
+		for (int64_t k = 0; k <= n; ++k)
+			sum += binomialCoefficient(int64_t(1), n, k);
+		EXPECT_EQ(int64_t(1) << n, sum);
+	}
+}
+
+TEST(BinomialCoefficientTest, Double)
+{
+	EXPECT_DOUBLE_EQ(2.7028824094543656951561469362597527549615200844e299, binomialCoefficient(1.0, 1000, 500));
+}
