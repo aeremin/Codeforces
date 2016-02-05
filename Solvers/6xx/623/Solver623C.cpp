25a0e86bfcb5a073a0d93de1825fa00da14b5a5d
Alexey Eremin <a.eremin.msu@gmail.com>
2016-02-05 20:51:31 +0300

    632C+

--- Solvers/6xx/623/Solver623C.cpp
+++ Solvers/6xx/623/Solver623C.cpp
@@ -1,12 +1,8 @@
 #include <Solvers/pch.h>
 #include "algo/geometry/geomvector.hpp"
 #include "algo/io/readvector.hpp"
-#include "algo/binaryfunctors/Min.hpp"
-#include "algo/binaryfunctors/Max.hpp"
-#include "algo/updatetypes/SetTo.hpp"
-#include "algo/updateappliers/SetToIdempotent.h"
-#include "algo/SegmentTree.hpp"
 #include "algo/FunctorIterator.hpp"
+#include "algo/sqr.hpp"
 
 using namespace std;
 
@@ -20,45 +16,77 @@ void Solver623C::run()
 {
     size_t nPts;
     cin >> nPts;
-    auto pts = readVector<GeomVector2I>( nPts );
-    sort( begin( pts ), end( pts ) );
-    vector<int> x, y;
-    for (auto pt : pts )
+    auto pts = readVector<GeomVector2I>(nPts);
+    sort(begin(pts), end(pts));
+    vector<int64_t> x, y;
+    for (auto pt : pts)
     {
-        x.push_back( pt[0] );
-        y.push_back( pt[1] );
+        x.push_back(pt[0]);
+        y.push_back(pt[1]);
     }
 
-    auto maxSegmentTree = makeSegmentTree( y, binaryFunctors::Max<int>(), updateTypes::SetTo<int>() );
-    auto minSegmentTree = makeSegmentTree( y, binaryFunctors::Min<int>(), updateTypes::SetTo<int>() );
+    vector<pair<int64_t, int64_t>> minMaxPrefixes(nPts), minMaxSuffixes(nPts);
+    minMaxPrefixes.front() = { y.front(), y.front() };
+    minMaxSuffixes.back() =  { y.back(), y.back() };
+    for (size_t i = 1; i < nPts; ++i)
+    {
+        minMaxPrefixes[i] = { min(y[i], minMaxPrefixes[i - 1].first), max(y[i], minMaxPrefixes[i - 1].second) };
+        minMaxSuffixes[nPts - 1 - i] = { min(y[nPts - 1 - i], minMaxSuffixes[nPts - i].first), max(y[nPts - 1 - i], minMaxSuffixes[nPts - i].second) };
+    }
 
-    int64_t fullYDiam = maxSegmentTree.getValueOnSegment( 0, nPts ) - minSegmentTree.getValueOnSegment( 0, nPts );
+    auto getMinMaxY = [&]( int l, int r )  -> pair<int64_t, int64_t>
+    { 
+        if (r == l)
+            return { 0, 0 };
+        if (l == 0)
+            return minMaxPrefixes[r - 1];
+        else
+            return minMaxSuffixes[l];
+    };
+
+    
+    auto fullMinMaxY = getMinMaxY(0, nPts);
+
+    int64_t fullYDiam = fullMinMaxY.second - fullMinMaxY.first;
     int64_t fullXDiam = x.back() - x.front();
-
-    auto getMaxY = [&]( int l, int r )  {   return ( r > l ) ? maxSegmentTree.getValueOnSegment( l, r ) : 0;  };
-    auto getMinY = [&]( int l, int r )  {   return ( r > l ) ? minSegmentTree.getValueOnSegment( l, r ) : 0;  };
+    int64_t fullDiam = min(fullXDiam, fullYDiam);
 
     auto canHaveDiameterLessOrEqual = [&]( int64_t diamSquared )->int
     {
         int64_t diam = sqrt( diamSquared );
 
-        if ( fullYDiam <= diam || fullXDiam <= diam )
+        if (fullDiam <= diam)
             return 1;
 
         for ( auto xc : x )
         {
-            auto xl = lower_bound( begin( x ), end( x ), xc - abs(xc) ) - begin( x );
-            auto xr = upper_bound( begin( x ), end( x ), xc + abs(xc) ) - begin( x );
-            auto yMax = max( getMaxY( 0, xl ), getMaxY( xr, nPts ) );
-            auto yMin = min( getMinY( 0, xl ), getMinY( xr, nPts ) );
-            if ( max( { yMax * yMax + xc * xc, yMin * yMin + xc * xc, ( yMax - yMin ) * ( yMax - yMin ) } ) <= diamSquared )
+            int64_t xMax, xMin;
+            if (xc > 0)
+            {
+                xMax = xc;
+                xMin = max(-xc, xc - diam);
+            }
+            else
+            {
+                xMin = xc;
+                xMax = min(-xc, xc + diam);
+            }
+            
+            auto xl = lower_bound( begin( x ), end( x ), xMin ) - begin( x );
+            auto xr = upper_bound( begin( x ), end( x ), xMax ) - begin( x );
+            
+            auto minMaxYLeft  = getMinMaxY(0, xl);
+            auto minMaxYRight = getMinMaxY(xr, nPts);
+            auto yMax = max(minMaxYLeft.second, minMaxYRight.second);
+            auto yMin = min(minMaxYLeft.first, minMaxYRight.first);
+            if ( max( { sqr(yMax) + sqr(xc), sqr(yMin) + sqr(xc), sqr(yMax - yMin) } ) <= diamSquared )
                 return 1;
         }
         return 0;
     };
 
     auto l = makeFunctorIterator( int64_t(0), canHaveDiameterLessOrEqual );
-    auto r = makeFunctorIterator( numeric_limits<int64_t>::max(), canHaveDiameterLessOrEqual );
+    auto r = makeFunctorIterator( sqr(fullDiam) + 1, canHaveDiameterLessOrEqual );
     auto res = lower_bound( l, r, 1 ).getParameter();
     cout << res;
 }
