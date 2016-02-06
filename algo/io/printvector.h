05f2b66e2b4d442f136bf5446a996d865c27bf93
Alexey Eremin <a.eremin.msu@gmail.com>
2016-02-06 17:48:38 +0300

    Fix printVector crash for empty vectors

--- algo/io/printvector.h
+++ algo/io/printvector.h
@@ -5,6 +5,8 @@
 template<typename T>
 void printVector(const std::vector<T>& out, std::string delimiter = " ")
 {
+    if (out.empty())
+        return;
     for (size_t i = 0; i < out.size() - 1; ++i)
         std::cout << out[i] << delimiter;
     std::cout << out.back();
