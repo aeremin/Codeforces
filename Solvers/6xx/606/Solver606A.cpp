055dbc3866c6918f43c89ae9c647b810fe2c09ed
Alexey Eremin <a.eremin.msu@gmail.com>
2016-02-07 12:03:46 +0300

    606A+

---
+++ Solvers/6xx/606/Solver606A.cpp
@@ -0,0 +1,69 @@
+#include <Solvers/pch.h>
+#include "algo/io/readvector.hpp"
+
+using namespace std;
+
+class Solver606A
+{
+public:
+    void run();
+};
+
+void Solver606A::run()
+{
+    auto has = readVector<int>(3);
+    auto need = readVector<int>(3);
+    int balance = 0;
+    for (int i = 0; i < 3; ++i)
+    {
+        auto diff = has[i] - need[i];
+        balance += (diff > 0) ? diff / 2 : diff;
+    }
+
+    cout << ((balance >= 0) ? "Yes" : "No");
+}
+
+class Solver606ATest : public ProblemTest {};
+
+TEST_F(Solver606ATest, Example1)
+{
+    string input = R"(4 4 0
+2 1 2
+)";
+    string output = R"(Yes
+)";
+    output.pop_back();
+    setInput(input);
+    Solver606A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver606ATest, Example2)
+{
+    string input = R"(5 6 1
+2 7 2
+)";
+    string output = R"(No
+)";
+    output.pop_back();
+    setInput(input);
+    Solver606A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver606ATest, Example3)
+{
+    string input = R"(3 3 3
+2 2 2
+)";
+    string output = R"(Yes
+)";
+    output.pop_back();
+    setInput(input);
+    Solver606A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
