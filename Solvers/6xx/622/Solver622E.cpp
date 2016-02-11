5f93b905b35fa2da68a71d9c00b7f27dd370790b
Alexey Eremin <a.eremin.msu@gmail.com>
2016-02-11 17:25:26 +0300

    622E+, 622F+

---
+++ Solvers/6xx/622/Solver622E.cpp
@@ -0,0 +1,102 @@
+#include <Solvers/pch.h>
+#include "algo/graph/Graph.hpp"
+#include "algo/Relax.hpp"
+
+using namespace std;
+
+class Solver622E
+{
+public:
+    void run();
+
+    vector<int> leavesDepths;
+    unique_ptr<SimpleGraph> g;
+    int depth = 1;
+    void dfs(int v, int p)
+    {
+        bool isLeaf = true;
+        for (auto pNei : g->vertexNeighbors(v))
+        {
+            if (pNei.first == p) continue;
+            isLeaf = false;
+            ++depth;
+            dfs(pNei.first, v);
+            --depth;
+        }
+
+        if (isLeaf)
+            leavesDepths.push_back(depth);
+    }
+};
+
+void Solver622E::run()
+{
+    int nVertices;
+    cin >> nVertices;
+    g.reset(new SimpleGraph(nVertices));
+    for (int i = 0; i < nVertices - 1; ++i)
+    {
+        int a, b;
+        cin >> a >> b;
+        g->addEdge(a - 1, b - 1);
+    }
+
+    int ans = 0;
+    
+    for (auto pNei : g->vertexNeighbors(0))
+    {
+        leavesDepths.clear();
+        dfs(pNei.first, 0);
+        sort(begin(leavesDepths), end(leavesDepths));
+        int subAns = 0;
+        for (auto d : leavesDepths)
+        {
+            ++subAns;
+            relaxMax(subAns, d);
+        }
+        relaxMax(ans, subAns);
+    }
+
+    cout << ans;
+}
+
+class Solver622ETest : public ProblemTest {};
+
+TEST_F(Solver622ETest, Example1)
+{
+    string input = R"(12
+1 2
+1 3
+1 4
+2 5
+2 6
+3 7
+3 8
+3 9
+8 10
+8 11
+8 12
+)";
+    string output = R"(6
+)";
+    output.pop_back();
+    setInput(input);
+    Solver622E().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver622ETest, Example2)
+{
+    string input = R"(2
+2 1
+)";
+    string output = R"(1
+)";
+    output.pop_back();
+    setInput(input);
+    Solver622E().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
---
+++ Solvers/6xx/622/Solver622F.cpp
@@ -0,0 +1,118 @@
+#include <Solvers/pch.h>
+#include "algo/numbertheory/Residue.hpp"
+#include "algo/algebra/QuickPower.hpp"
+
+using namespace std;
+
+class Solver622F
+{
+public:
+    void run();
+
+    CodeforcesResidue64 res(int n, int k)
+    {
+        vector<CodeforcesResidue64> factorials = { 1 };
+        for (int64_t i = 1; i <= k + 1; ++i)
+            factorials.push_back(factorials.back() * i);
+
+        vector<CodeforcesResidue64> inversedFactorials;
+        for (auto f : factorials)
+            inversedFactorials.push_back(f.inversed());
+
+        vector<CodeforcesResidue64> values = { 0 };
+        for (int64_t i = 1; i <= k + 1; ++i)
+            values.push_back(values.back() + quickPower(CodeforcesResidue64(i), k));
+
+        if (n <= k + 1)
+            return values[n];
+
+        CodeforcesResidue64 ans = 0;
+
+        CodeforcesResidue64 numerator = 1;
+        for (int64_t i = 0; i <= k + 1; ++i)
+            numerator *= (n - i);
+
+        for (int64_t i = 0; i <= k + 1; ++i)
+        {
+            int64_t sign = (k + 1 - i) % 2 == 0 ? 1 : -1;
+            ans += numerator * CodeforcesResidue64(n - i).inversed() * inversedFactorials[i] * inversedFactorials[k + 1 - i] * sign * values[i];
+        }
+        return ans;
+    }
+};
+
+void Solver622F::run()
+{
+    int n, k;
+    cin >> n >> k;
+    cout << res(n, k);
+}
+
+class Solver622FTest : public ProblemTest {};
+
+TEST_F(Solver622FTest, Example1)
+{
+    string input = R"(4 1
+)";
+    string output = R"(10
+)";
+    output.pop_back();
+    setInput(input);
+    Solver622F().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver622FTest, Example2)
+{
+    string input = R"(4 2
+)";
+    string output = R"(30
+)";
+    output.pop_back();
+    setInput(input);
+    Solver622F().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver622FTest, Example3)
+{
+    string input = R"(4 3
+)";
+    string output = R"(100
+)";
+    output.pop_back();
+    setInput(input);
+    Solver622F().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver622FTest, Example4)
+{
+    string input = R"(4 0
+)";
+    string output = R"(4
+)";
+    output.pop_back();
+    setInput(input);
+    Solver622F().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver622FTest, CompareWithBruteForce)
+{
+    for (int k = 0; k < 100; ++k)
+    {
+        CodeforcesResidue64 bruteForceAnswer = 0;
+        for (int n = 1; n <= 100; ++n)
+        {
+            bruteForceAnswer += quickPower(CodeforcesResidue64(n), k);
+            ASSERT_EQ(bruteForceAnswer, Solver622F().res(n, k)) << n << " " << k;
+        }
+    }
+}
+
+
