4694ebccd96d2993d2d45c9bc95a914c7425741f
Alexey Eremin <a.eremin.msu@gmail.com>
2016-02-13 13:39:55 +0300

    Add 604A solution as an example of new IO usage

---
+++ Solvers/6xx/604/Solver604A.cpp
@@ -0,0 +1,59 @@
+#include <Solvers/pch.h>
+#include "algo/range.hpp"
+#include "algo/io/readvector.hpp"
+using namespace std;
+
+class Solver604A
+{
+public:
+    void run();
+};
+
+void Solver604A::run()
+{
+    auto times = readVector<int>(5);
+    auto tries = readVector<int>(5);
+    auto successfulHacks = read<int>();
+    auto unsuccessfulHacks = read<int>();
+    
+    int res = 100 * successfulHacks - 50 * unsuccessfulHacks;
+    for (int i : range(5))
+    {
+        auto problemCostBy250 = (i + 1) * 2;
+        res += max(75 * problemCostBy250, (250 - times[i]) * problemCostBy250 - 50 * tries[i]);
+    }
+    print(res);
+}
+
+class Solver604ATest : public ProblemTest {};
+
+TEST_F(Solver604ATest, Example1)
+{
+    string input = R"(20 40 60 80 100
+0 1 2 3 4
+1 0
+)";
+    string output = R"(4900
+)";
+    output.pop_back();
+    setInput(input);
+    Solver604A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver604ATest, Example2)
+{
+    string input = R"(119 119 119 119 119
+0 0 0 0 0
+10 0
+)";
+    string output = R"(4930
+)";
+    output.pop_back();
+    setInput(input);
+    Solver604A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
