04c5c79de1f9ed1526d094697b80a2d5d2dc3b45
Andrei Matveiakin <a.matveyakin@gmail.com>
2016-02-18 15:08:03 +0100

    Add solutions of 573A and 573B

---
+++ Solvers/5xx/573/Solver573A.cpp
@@ -0,0 +1,69 @@
+#include <Solvers/pch.h>
+#include "algo/io/baseio.hpp"
+#include "algo/range.hpp"
+#include "algo/io/readvector.hpp"
+#include "algo/numbertheory/GreatestCommonDivisor.hpp"
+
+using namespace std;
+
+class Solver573A
+{
+public:
+    void run();
+
+    void eliminate2and3(int& a)
+    {
+        while (a % 2 == 0) a /= 2;
+        while (a % 3 == 0) a /= 3;
+    }
+};
+
+void Solver573A::run()
+{
+    int n = read<int>();
+    auto bets = readVector<int>(n);
+    bool can = true;
+    for (int i = 1; i < n; ++i)
+    {
+        auto d = greatestCommonDivisor(bets[0], bets[i]);
+        int b1 = bets[0] / d;
+        int b2 = bets[i] / d;
+        eliminate2and3(b1);
+        eliminate2and3(b2);
+        can = can && b1 == 1 && b2 == 1;
+    }
+
+    if (can) cout << "Yes";
+        else cout << "No";
+}
+
+class Solver573ATest : public ProblemTest {};
+
+TEST_F(Solver573ATest, Example1)
+{
+    string input = R"(4
+75 150 75 50
+)";
+    string output = R"(Yes
+)";
+    output.pop_back();
+    setInput(input);
+    Solver573A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver573ATest, Example2)
+{
+    string input = R"(3
+100 150 250
+)";
+    string output = R"(No
+)";
+    output.pop_back();
+    setInput(input);
+    Solver573A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
---
+++ Solvers/5xx/573/Solver573B.cpp
@@ -0,0 +1,64 @@
+#include <Solvers/pch.h>
+#include "algo/io/baseio.hpp"
+#include "algo/io/readvector.hpp"
+#include "algo/range.hpp"
+#include "algo/Relax.hpp"
+using namespace std;
+
+class Solver573B
+{
+public:
+    void run();
+};
+
+void Solver573B::run()
+{
+    int n = read<int>();
+    auto h = readVector<int>(n);
+    vector<int> maxL(n), maxR(n);
+
+    maxL[0] = 1;
+    for (int i = 1; i < n; ++i)
+        maxL[i] = min(h[i], maxL[i - 1] + 1);
+
+    maxR[n - 1] = 1;
+    for (int i = n - 2; i >= 0; --i)
+        maxR[i] = min(h[i], maxR[i + 1] + 1);
+
+    int ans = 0;
+    for (int i = 0; i < n; ++i)
+        relaxMax(ans, min(maxL[i], maxR[i]));
+
+    cout << ans;
+}
+
+class Solver573BTest : public ProblemTest {};
+
+TEST_F(Solver573BTest, Example1)
+{
+    string input = R"(6
+2 1 4 6 2 2
+)";
+    string output = R"(3
+)";
+    output.pop_back();
+    setInput(input);
+    Solver573B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver573BTest, Example2)
+{
+    string input = R"(7
+3 3 3 1 3 3 3
+)";
+    string output = R"(2
+)";
+    output.pop_back();
+    setInput(input);
+    Solver573B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
