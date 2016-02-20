2d4ab0c9336d52c07a7d0729aff2b479b4d17c1e
Andrei Matveiakin <a.matveyakin@gmail.com>
2016-02-20 22:59:42 +0100

    Move trim functions to //string/

--- compiler/file_parser.cpp
+++ compiler/file_parser.cpp
@@ -3,22 +3,11 @@
 #include <fstream>
 #include <regex>
 
+#include "strings/trim.h"
 #include "util/check.h"
 
 
 namespace {
-
-inline std::string &ltrim(std::string &s) {
-  s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
-  return s;
-}
-inline std::string &rtrim(std::string &s) {
-  s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-  return s;
-}
-inline std::string &trim(std::string &s) {
-  return ltrim(rtrim(s));
-}
 
 std::regex pragma_once_regex("#pragma +once");
 std::regex local_include_regex("#include *\"(.*)\"");
