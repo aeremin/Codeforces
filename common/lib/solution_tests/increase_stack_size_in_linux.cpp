0f404726bfd67c5e87f76f96b357b45e21616244
Andrei Matveiakin <a.matveyakin@gmail.com>
2016-02-25 00:58:44 +0100

    Increase stack size to 256 megabytes in Linux

---
+++ common/lib/solution_tests/increase_stack_size_in_linux.cpp
@@ -0,0 +1,33 @@
+// Increases stack size under GNU/Linux operating systems.
+//
+// You only need to *link* this file.
+
+#include <iostream>
+
+#include <sys/resource.h>
+
+#include "util/check.h"
+
+
+namespace {
+
+#ifndef STACK_SIZE_MB
+#   define STACK_SIZE_MB  256
+#endif
+
+class StackEnlarger {
+public:
+    StackEnlarger() {
+        constexpr rlim_t desired_stack_size = STACK_SIZE_MB * 1024 * 1024;
+        rlimit rl;
+        CHECK(getrlimit(RLIMIT_STACK, &rl) == 0);
+        if (rl.rlim_cur < desired_stack_size) {
+            rl.rlim_cur = desired_stack_size;
+            CHECK(setrlimit(RLIMIT_STACK, &rl) == 0);
+        }
+    }
+};
+
+StackEnlarger stack_enlarger;
+
+}  // namespace
