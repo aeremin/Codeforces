ee3b26225f5b2a8d72eeb9d7ad4ae31d123898c1
Alexey Eremin <a.eremin.msu@gmail.com>
2016-02-29 20:26:04 +0300

    627B+

---
+++ alexey/Solvers/6xx/627/Solver627B.cpp
@@ -0,0 +1,99 @@
+#include <Solvers/pch.h>
+#include "algo/io/baseio.hpp"
+#include "algo/updatetypes/IncreaseBy.hpp"
+#include "algo/updateappliers/IncreaseBySum.h"
+#include "algo/SegmentTree.hpp"
+#include "iter/range.h"
+#include "algo/io/printvector.h"
+
+using namespace std;
+
+class Solver627B
+{
+public:
+    void run();
+};
+
+void Solver627B::run()
+{
+    int nDays, repairLen, produceBeforeRepair, produceAfterRepair, nQueries;
+    cin >> nDays >> repairLen >> produceAfterRepair >> produceBeforeRepair >> nQueries;
+    vector<int> result;
+    vector<int> nRequests(nDays, 0);
+    auto streeBefore = makeSegmentTree(nRequests, plus<int>(), updateTypes::IncreaseBy<int>());
+    auto streeAfter = makeSegmentTree(nRequests, plus<int>(), updateTypes::IncreaseBy<int>());
+    for (int i : range(nQueries))
+    {
+        int t;
+        cin >> t;
+        if (t == 1)
+        {
+            int d, a;
+            cin >> d >> a;
+            --d;
+            auto currBefore = streeBefore.getValueOnSegment(d, d + 1);
+            auto incrBefore = min(a, produceBeforeRepair - currBefore);
+            streeBefore.updateElement(d, updateTypes::IncreaseBy<int>(incrBefore));
+         
+            auto currAfter = streeAfter.getValueOnSegment(d, d + 1);
+            auto incrAfter = min(a, produceAfterRepair - currAfter);
+            streeAfter.updateElement(d, updateTypes::IncreaseBy<int>(incrAfter));
+        }
+        else
+        {
+            int p;
+            cin >> p;
+            --p;
+            auto fullfillBeforeRepair = (p > 0) ? streeBefore.getValueOnSegment(0, p) : 0;
+            auto fullfillAfterRepair = (nDays > p + repairLen) ? streeAfter.getValueOnSegment(p + repairLen, nDays) : 0;
+            result.push_back(fullfillBeforeRepair + fullfillAfterRepair);
+        }
+    }
+    printVector(result, "\n");
+}
+
+class Solver627BTest : public ProblemTest {};
+
+TEST_F(Solver627BTest, Example1)
+{
+    string input = R"(5 2 2 1 8
+1 1 2
+1 5 3
+1 2 1
+2 2
+1 4 2
+1 3 2
+2 1
+2 3
+)";
+    string output = R"(3
+6
+4
+)";
+    output.pop_back();
+    setInput(input);
+    Solver627B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver627BTest, Example2)
+{
+    string input = R"(5 4 10 1 6
+1 1 5
+1 5 5
+1 3 2
+1 5 2
+2 1
+2 2
+)";
+    string output = R"(7
+1
+)";
+    output.pop_back();
+    setInput(input);
+    Solver627B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
---
+++ alexey/algo/updateappliers/IncreaseBySum.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "algo/updateappliers/UpdateApplier.hpp"
+
+template<typename T>
+struct UpdateApplier<updateTypes::IncreaseBy<T>, std::plus<T>>
+{
+    static T apply(const T& v, const updateTypes::IncreaseBy<T>& upd, size_t cnt)
+    {
+        return v + cnt * upd.value_;
+    }
+};
