11c6f579d131f2bb3f3a0fc2aaeed54c194ab86d
Alexey Eremin <a.eremin.msu@gmail.com>
2016-03-01 16:10:28 +0300

    634A+

---
+++ alexey/Solvers/6xx/634/Solver634A.cpp
@@ -0,0 +1,91 @@
+#include <Solvers/pch.h>
+#include "algo/io/baseio.hpp"
+#include "iter/range.h"
+#include "algo/io/readvector.hpp"
+using namespace std;
+
+class Solver634A
+{
+public:
+    void run();
+
+    vector<int> normalize( const vector<int>& in )
+    {
+        vector<int> result;
+        auto firstStatueIter = find( begin( in ), end( in ), 1 );
+        auto collectIfNotZero = [&]( int val )
+        {
+            if ( val != 0 )
+                result.push_back( val );
+        };
+
+        for ( auto it = firstStatueIter; it != end( in ); ++it )
+            collectIfNotZero( *it );
+
+        for ( auto it = begin( in ); it != firstStatueIter; ++it )
+            collectIfNotZero( *it );
+
+        return result;
+    }
+};
+
+void Solver634A::run()
+{
+    int n = read<int>();
+    auto before = readVector<int>( n );
+    auto after = readVector<int>( n );
+    before = normalize( before );
+    after = normalize( after );
+    if ( after == before )
+        cout << "YES";
+    else
+        cout << "NO";
+}
+
+class Solver634ATest : public ProblemTest {};
+
+TEST_F(Solver634ATest, Example1)
+{
+    string input = R"(3
+1 0 2
+2 0 1
+)";
+    string output = R"(YES
+)";
+    output.pop_back();
+    setInput(input);
+    Solver634A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver634ATest, Example2)
+{
+    string input = R"(2
+1 0
+0 1
+)";
+    string output = R"(YES
+)";
+    output.pop_back();
+    setInput(input);
+    Solver634A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver634ATest, Example3)
+{
+    string input = R"(4
+1 2 3 0
+0 3 2 1
+)";
+    string output = R"(NO
+)";
+    output.pop_back();
+    setInput(input);
+    Solver634A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
