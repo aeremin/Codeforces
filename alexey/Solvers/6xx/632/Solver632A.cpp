455516eceb922303c4192ff146d3e3518b6e84cf
Alexey Eremin <a.eremin.msu@gmail.com>
2016-03-01 20:19:01 +0300

    632A+, 632B+

---
+++ alexey/Solvers/6xx/632/Solver632A.cpp
@@ -0,0 +1,74 @@
+#include <Solvers/pch.h>
+#include "algo/io/baseio.hpp"
+#include "iter/range.h"
+using namespace std;
+
+class Solver632A
+{
+public:
+    void run();
+};
+
+void Solver632A::run()
+{
+    int nBuyers;
+    int price;
+    cin >> nBuyers >> price;
+    vector<bool> isBonus;
+    int totalBonus = 0;
+    for ( int i : range( nBuyers ) )
+    {
+        string s;
+        cin >> s;
+        bool isb = ( s == "halfplus" );
+        isBonus.push_back( isb );
+        if ( isb )
+            totalBonus++;
+    }
+
+    int64_t totalApples = 0;
+    for ( int i = nBuyers - 1; i >= 0; --i )
+    {
+        if ( isBonus[i] )
+            totalApples = 2 * totalApples + 1;
+        else
+            totalApples = 2 * totalApples;
+    }
+
+    int64_t totalPaidHalves = 2 * totalApples - totalBonus;
+    cout << ( price / 2 ) * totalPaidHalves;
+}
+
+class Solver632ATest : public ProblemTest {};
+
+TEST_F(Solver632ATest, Example1)
+{
+    string input = R"(2 10
+half
+halfplus
+)";
+    string output = R"(15
+)";
+    output.pop_back();
+    setInput(input);
+    Solver632A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver632ATest, Example2)
+{
+    string input = R"(3 10
+halfplus
+halfplus
+halfplus
+)";
+    string output = R"(55
+)";
+    output.pop_back();
+    setInput(input);
+    Solver632A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
---
+++ alexey/Solvers/6xx/632/Solver632B.cpp
@@ -0,0 +1,92 @@
+#include <Solvers/pch.h>
+#include "algo/io/baseio.hpp"
+#include "iter/range.h"
+#include "algo/io/readvector.hpp"
+#include "util/relax.h"
+using namespace std;
+
+class Solver632B
+{
+public:
+    void run();
+};
+
+void Solver632B::run()
+{
+    int n = read<int>();
+    auto pows = readVector<int64_t>( n );
+    auto ownership = readVector<char>( n );
+
+    int64_t startBobPower = 0;
+    for ( int i : range( n ) )
+        if ( ownership[i] == 'B' )
+            startBobPower += pows[i];
+
+    int64_t currDiff = 0;
+    int64_t maxPow = startBobPower;
+    for ( int i : range( n ) )
+    {
+        int sgn = ( ownership[i] == 'B' ) ? -1 : 1;
+        currDiff += sgn * pows[i];
+        relax_max( maxPow, startBobPower + currDiff );
+    }
+
+    currDiff = 0;
+    for ( int j : range( n ) )
+    {
+        int i = n - 1 - j;
+        int sgn = ( ownership[i] == 'B' ) ? -1 : 1;
+        currDiff += sgn * pows[i];
+        relax_max( maxPow, startBobPower + currDiff );
+    }
+
+    cout << maxPow;
+}
+
+class Solver632BTest : public ProblemTest {};
+
+TEST_F(Solver632BTest, Example1)
+{
+    string input = R"(5
+1 2 3 4 5
+ABABA
+)";
+    string output = R"(11
+)";
+    output.pop_back();
+    setInput(input);
+    Solver632B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver632BTest, Example2)
+{
+    string input = R"(5
+1 2 3 4 5
+AAAAA
+)";
+    string output = R"(15
+)";
+    output.pop_back();
+    setInput(input);
+    Solver632B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver632BTest, Example3)
+{
+    string input = R"(1
+1
+B
+)";
+    string output = R"(1
+)";
+    output.pop_back();
+    setInput(input);
+    Solver632B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
