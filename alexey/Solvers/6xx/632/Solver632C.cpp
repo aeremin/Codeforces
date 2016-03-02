e55bf904e5551f8b633167e8d3794b42f8ee5d6e
Alexey Eremin <a.eremin.msu@gmail.com>
2016-03-02 11:55:06 +0300

    632C+

---
+++ alexey/Solvers/6xx/632/Solver632C.cpp
@@ -0,0 +1,78 @@
+#include <Solvers/pch.h>
+#include "algo/io/baseio.hpp"
+#include "iter/range.h"
+#include "algo/io/readvector.hpp"
+#include "algo/io/printvector.h"
+using namespace std;
+
+class Solver632C
+{
+public:
+    void run();
+};
+
+void Solver632C::run()
+{
+    auto n = read<int>();
+    auto words = readVector<string>(n);
+    auto cmp = [](const string& a, const string& b) {
+        return a + b < b + a;
+    };
+
+    sort(begin(words), end(words), cmp);
+    printVector(words, "");
+}
+
+class Solver632CTest : public ProblemTest {};
+
+TEST_F(Solver632CTest, Example1)
+{
+    string input = R"(4
+abba
+abacaba
+bcd
+er
+)";
+    string output = R"(abacabaabbabcder
+)";
+    output.pop_back();
+    setInput(input);
+    Solver632C().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver632CTest, Example2)
+{
+    string input = R"(5
+x
+xx
+xxa
+xxaa
+xxaaa
+)";
+    string output = R"(xxaaaxxaaxxaxxx
+)";
+    output.pop_back();
+    setInput(input);
+    Solver632C().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver632CTest, Example3)
+{
+    string input = R"(3
+c
+cb
+cba
+)";
+    string output = R"(cbacbc
+)";
+    output.pop_back();
+    setInput(input);
+    Solver632C().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
