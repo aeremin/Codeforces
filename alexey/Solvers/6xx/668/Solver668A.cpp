27c0bf910104c767b7972321c8aeb4c71913a722
Alexey Eremin <a.eremin.msu@gmail.com>
2016-04-24 23:55:19 +0300

    Add solutions of 668A and 668B

---
+++ alexey/Solvers/6xx/668/Solver668A.cpp
@@ -0,0 +1,87 @@
+#include <Solvers/pch.h>
+#include "algo/io/baseio.hpp"
+#include "iter/range.h"
+#include "algo/io/printvector.h"
+using namespace std;
+
+class Solver668A {
+public:
+    void run();
+};
+
+void Solver668A::run() {
+    int n, m, q;
+    cin >> n >> m >> q;
+    
+    struct Query { int a, b, c, d; };
+    deque<Query> queries;
+
+    for (int i : range(q)) {
+        int a, b;
+        cin >> a >> b;
+        int c = 1;
+        int d = 0;
+        if (a == 3)
+            cin >> c >> d;
+        queries.push_front({ a, b - 1, c - 1, d });
+    }
+
+    vector<vector<int>> field(n, vector<int>(m));
+    for (auto q : queries) {
+        if (q.a == 1) {
+            auto t = field[q.b][m - 1];
+            for (int i = m - 1; i > 0; --i)
+                field[q.b][i] = field[q.b][i - 1];
+            field[q.b][0] = t;
+        }
+
+        if (q.a == 2) {
+            auto t = field[n - 1][q.b];
+            for (int i = n - 1; i > 0; --i)
+                field[i][q.b] = field[i - 1][q.b];
+            field[0][q.b] = t;
+        }
+        if (q.a == 3) {
+            field[q.b][q.c] = q.d;
+        }
+    }
+
+    for (auto& line : field) {
+        printVector(line);
+        print('\n');
+    }
+}
+
+
+class Solver668ATest : public ProblemTest {};
+
+
+TEST_F(Solver668ATest, Example1) {
+    string input = R"(2 2 6
+2 1
+2 2
+3 1 1 1
+3 2 2 2
+3 1 2 8
+3 2 1 8
+)";
+    string output = R"(8 2
+1 8
+)";
+    setInput(input);
+    Solver668A().run();
+    EXPECT_EQ(output, getOutput());
+}
+TEST_F(Solver668ATest, Example2) {
+    string input = R"(3 3 2
+1 2
+3 2 2 5
+)";
+    string output = R"(0 0 0
+0 0 5
+0 0 0
+)";
+    setInput(input);
+    Solver668A().run();
+    EXPECT_EQ(output, getOutput());
+}
---
+++ alexey/Solvers/6xx/668/Solver668B.cpp
@@ -0,0 +1,167 @@
+#include <Solvers/pch.h>
+#include "algo/io/baseio.hpp"
+#include "iter/range.h"
+#include "algo/io/printvector.h"
+using namespace std;
+
+class Solver668B {
+public:
+    void run();
+
+    void rotate(vector<int>& v, int64_t a) {
+        int64_t n = v.size();
+        a = a % n;
+        a += n;
+        a = a % n;
+        if (a == 0)
+            return;
+        vector<char> rotated(n, false);
+        for (auto i : range(n)) {
+            if (rotated[i])
+                continue;
+            auto t = v[i];
+            int prev = (i - a + n) % n;
+            int curr = i;
+            do {
+                v[curr] = v[prev];
+                rotated[curr] = true;
+                curr = prev;
+                prev = (curr - a + n) % n;
+            } while (prev != i);
+            v[curr] = t;
+            rotated[curr] = true;
+        }
+    }
+   
+    int64_t fixedModDiv(int64_t a, int64_t b) {
+        auto res = a % b;
+        res += b;
+        res = res % b;
+        return (a - res) / b;
+    }
+};
+
+void Solver668B::run() {
+    int n, q;
+    cin >> n >> q;
+    vector<pair<int, int64_t>> queries = { { 1, 0 } };
+    for (int i : range(q)) {
+        int t = read<int>();
+        if (t == 1) {
+            int s = read<int>();
+            if (queries.back().first == 1)
+                queries.back().second += s;
+            else
+                queries.push_back({ 1, s });
+
+            if (queries.size() > 1) {
+                queries.front().second += 2 * fixedModDiv(queries.back().second, 2);
+                if (queries.back().second % 2 == 0)
+                    queries.pop_back();
+                else
+                    queries.back().second = 1;
+            }
+        }
+        else {
+            if (queries.back().first == 2)
+                queries.pop_back();
+            else
+                queries.push_back({ 2, 0 });
+        }
+    }
+
+    vector<int> positions(n);
+    iota(begin(positions), end(positions), 1);
+
+    int k = count(begin(queries), end(queries), make_pair( 2, int64_t(0) ));
+
+    rotate(positions, queries.front().second);
+
+    vector<int> oddPositions, evenPositions;
+    for (int i : range(n / 2)) {
+        evenPositions.push_back(positions[2 * i]);
+        oddPositions.push_back(positions[2 * i + 1]);
+    }
+
+    rotate(evenPositions, k);
+
+    for (int i : range(n / 2)) {
+        positions[2 * i] = evenPositions[i];
+        positions[2 * i + 1] = oddPositions[i];
+    }
+
+    if (queries.back().first == 2)
+        rotate(positions, -1);
+
+    for (auto p : positions) {
+        print(p);
+        print(' ');
+    }
+}
+
+
+class Solver668BTest : public ProblemTest {};
+
+TEST_F(Solver668BTest, Rotate) {
+    vector<int> a = { 1, 2, 3, 4, 5, 6 };
+    Solver668B().rotate(a, -2);
+    vector<int> b = { 3, 4, 5, 6, 1, 2 };
+    EXPECT_EQ(a, b);
+}
+
+TEST_F(Solver668BTest, Rotate2) {
+    vector<int> a = { 1, 2, 3, 4, 5, 6 };
+    Solver668B().rotate(a, -666);
+    vector<int> b = { 1, 2, 3, 4, 5, 6 };
+    EXPECT_EQ(a, b);
+}
+
+TEST_F(Solver668BTest, Rotate3) {
+    vector<int> a = { 1, 2, 3, 4, 5, 6 };
+    Solver668B().rotate(a, 666);
+    vector<int> b = { 1, 2, 3, 4, 5, 6 };
+    EXPECT_EQ(a, b);
+}
+
+
+TEST_F(Solver668BTest, Example1) {
+    string input = R"(6 3
+1 2
+2
+1 2
+)";
+    string output = R"(4 3 6 5 2 1 
+)";
+    output.pop_back();
+    setInput(input);
+    Solver668B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+
+TEST_F(Solver668BTest, Example2) {
+    string input = R"(2 3
+1 1
+2
+1 -2
+)";
+    string output = R"(1 2 
+)";
+    output.pop_back();
+    setInput(input);
+    Solver668B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+TEST_F(Solver668BTest, Example3) {
+    string input = R"(4 2
+2
+1 3
+)";
+    string output = R"(1 4 3 2 
+)";
+    output.pop_back();
+    setInput(input);
+    Solver668B().run();
+    EXPECT_EQ(output, getOutput());
+}
