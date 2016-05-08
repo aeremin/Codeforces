a25a8462242f6d92ecfc581f7a13d544a7c550f5
Alexey Eremin <a.eremin.msu@gmail.com>
2016-05-09 02:56:09 +0300

    Add solution of problems 674A and 674B

---
+++ alexey/Solvers/6xx/674/Solver674A.cpp
@@ -0,0 +1,72 @@
+#include <Solvers/pch.h>
+#include "algo/io/baseio.hpp"
+#include "iter/range.h"
+#include "algo/io/readvector.hpp"
+#include "algo/io/printvector.h"
+using namespace std;
+
+class Solver674A {
+public:
+    void run();
+};
+
+void Solver674A::run() {
+    int n;
+    cin >> n;
+    auto colors = readVector<int>(n);
+
+    vector<vector<int>> colorOnPrefixCount(n, { 0 });
+
+    for (auto c : colors)
+        for (int iColor : range(n))
+            colorOnPrefixCount[iColor].push_back(colorOnPrefixCount[iColor].back()
+                                                 + (iColor == (c - 1)));
+
+    vector<int> ans(n);
+
+    for (int l : range(n)) {
+        int bestCnt = 0;
+        int bestColor = -1;
+
+        for (int r : range(l, n)) {
+            int candColor = colors[r] - 1;
+            int candColorCnt = colorOnPrefixCount[candColor][r + 1] - colorOnPrefixCount[candColor][l];
+
+            if (make_pair(candColorCnt, -candColor) > make_pair(bestCnt, -bestColor))
+                tie(bestCnt, bestColor) = make_pair(candColorCnt, candColor);
+
+            ++ans[bestColor];
+        }
+    }
+
+    printVector(ans);
+}
+
+
+class Solver674ATest : public ProblemTest {};
+
+
+TEST_F(Solver674ATest, Example1) {
+    string input = R"(4
+1 2 1 2
+)";
+    string output = R"(7 3 0 0
+)";
+    output.pop_back();
+    setInput(input);
+    Solver674A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+TEST_F(Solver674ATest, Example2) {
+    string input = R"(3
+1 1 1
+)";
+    string output = R"(6 0 0
+)";
+    output.pop_back();
+    setInput(input);
+    Solver674A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
---
+++ alexey/Solvers/6xx/674/Solver674B.cpp
@@ -0,0 +1,75 @@
+#include <Solvers/pch.h>
+#include "algo/io/baseio.hpp"
+#include "iter/range.h"
+#include "algo/io/printvector.h"
+using namespace std;
+
+class Solver674B {
+public:
+    void run();
+};
+
+void Solver674B::run() {
+    int n, k;
+    cin >> n >> k;
+    if (n < 5 || k <= n) {
+        cout << -1;
+        return;
+    }
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    vector<int> mapping(n);
+    mapping[0] = a;
+    mapping[n - 1] = b;
+    mapping[1] = c;
+    mapping[n - 2] = d;
+    int j = 1;
+    for (int i : range(2, n - 2)) {
+        while (j == a || j == b || j == c || j == d)
+            ++j;
+        mapping[i] = j;
+        ++j;
+    }
+
+
+    vector<int> ans(n);
+    iota(begin(ans), end(ans), 0);
+    for (auto elt : ans)
+        cout << mapping[elt] << ' ';
+    cout << '\n';
+    swap(ans[0], ans[1]);
+    swap(ans[n - 1], ans[n - 2]);
+    for (auto elt : ans)
+        cout << mapping[elt] << ' ';
+}
+
+
+class Solver674BTest : public ProblemTest {};
+
+
+TEST_F(Solver674BTest, Example1) {
+    string input = R"(7 11
+2 4 7 3
+)";
+    string output = R"(2 7 1 5 6 3 4 
+7 2 1 5 6 4 3 
+)";
+    output.pop_back();
+    setInput(input);
+    Solver674B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+TEST_F(Solver674BTest, Example2) {
+    string input = R"(1000 999
+10 20 30 40
+)";
+    string output = R"(-1
+)";
+    output.pop_back();
+    setInput(input);
+    Solver674B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
