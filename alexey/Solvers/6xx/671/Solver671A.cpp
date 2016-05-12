267d6be5ed351957495c598a1d34d374edb68185
Alexey Eremin <a.eremin.msu@gmail.com>
2016-05-12 19:50:19 +0300

    Add solutions of problems 671A and 671B

---
+++ alexey/Solvers/6xx/671/Solver671A.cpp
@@ -0,0 +1,98 @@
+#include <Solvers/pch.h>
+#include "algo/io/baseio.hpp"
+#include "iter/range.h"
+#include "algo/geometry/geomvector.hpp"
+#include "util/relax.h"
+using namespace std;
+
+class Solver671A {
+public:
+    void run();
+};
+
+void Solver671A::run() {
+    GeomVector2D p1, p2, t;
+    cin >> p1 >> p2 >> t;
+    int n = read<int>();
+    vector<GeomVector2D> bottles(n);
+    for (auto& b : bottles)
+        cin >> b;
+
+    double s = 0;
+    for (auto b : bottles)
+        s += 2 * sqrt((b - t).lengthSquared());
+
+    vector<pair<double, int>> v1(n), v2(n);
+    for (int i : range(n)) {
+        auto b = bottles[i];
+        v1[i] = { sqrt((p1 - b).lengthSquared()) - sqrt((t - b).lengthSquared()), i };
+        v2[i] = { sqrt((p2 - b).lengthSquared()) - sqrt((t - b).lengthSquared()), i };
+    }
+
+    sort(begin(v1), end(v1));
+    sort(begin(v2), end(v2));
+
+    double ans = numeric_limits<double>::max();
+    relax_min(ans, v1[0].first);
+    relax_min(ans, v2[0].first);
+    if (v1[0].second != v2[0].second) {
+        relax_min(ans, v1[0].first + v2[0].first);
+    }
+    else if (n > 1) {
+        relax_min(ans, v1[0].first + v2[1].first);
+        relax_min(ans, v1[1].first + v2[0].first);
+    }
+
+    ans += s;
+
+    cout << setprecision(12) << fixed << ans;
+}
+
+
+class Solver671ATest : public ProblemTest {};
+
+
+TEST_F(Solver671ATest, Example1) {
+    string input = R"(3 1 1 2 0 0
+3
+1 1
+2 1
+2 3
+)";
+    string output = R"(11.084259940083
+)";
+    output.pop_back();
+    setInput(input);
+    Solver671A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+TEST_F(Solver671ATest, Example2) {
+    string input = R"(5 0 4 2 2 0
+5
+5 2
+3 0
+5 5
+3 5
+3 3
+)";
+    string output = R"(33.121375178000
+)";
+    output.pop_back();
+    setInput(input);
+    Solver671A().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+TEST_F(Solver671ATest, Example3) {
+    string input = R"(5 0 5 0 0 0
+1
+0 0
+)";
+    string output = R"(5.000000000000
+)";
+    output.pop_back();
+    setInput(input);
+    Solver671A().run();
+    EXPECT_EQ(output, getOutput());
+}
---
+++ alexey/Solvers/6xx/671/Solver671B.cpp
@@ -0,0 +1,125 @@
+#include <Solvers/pch.h>
+#include "algo/io/baseio.hpp"
+#include "iter/range.h"
+#include "algo/io/readvector.hpp"
+#include "algo/FunctorIterator.hpp"
+using namespace std;
+
+class Solver671B {
+public:
+    void run();
+
+    vector<int64_t> coins;
+    vector<int64_t> coinsPrefixSums = { 0 };
+
+    int64_t n;
+
+    int64_t neededToSetMinTo(int v) const {
+        int64_t r = lower_bound(begin(coins), end(coins), v) - begin(coins);
+        int64_t s = coinsPrefixSums[r];
+        return r * v - s;
+    }
+
+
+    int64_t neededToSetMaxTo(int v) const {
+        int64_t l = upper_bound(begin(coins), end(coins), v) - begin(coins);
+        int64_t s = coinsPrefixSums[n] - coinsPrefixSums[l];
+        return s - (n - l) * v;
+    }
+};
+
+void Solver671B::run() {
+    int64_t k;
+    cin >> n >> k;
+    coins = readVector<int64_t>(n);
+    int64_t s = 0;
+    for (auto c : coins)
+        s += c;
+
+    sort(begin(coins), end(coins));
+    partial_sum(begin(coins), end(coins), back_inserter(coinsPrefixSums));
+
+    int64_t minConverge, maxConverge;
+    if (s % n == 0)
+        minConverge = maxConverge = s / n;
+    else {
+        minConverge = s / n;
+        maxConverge = s / n + 1;
+    }
+
+    auto minFunctor = [&](int64_t v) ->int {return neededToSetMinTo(v) > k; };
+    auto lMin = makeFunctorIterator(int64_t (-1), minFunctor);
+    auto rMin = makeFunctorIterator(int64_t(2e9 + 1), minFunctor);
+    auto minV = upper_bound(lMin, rMin, 0).getParameter() - 1;
+
+    auto maxFunctor = [&](int64_t v) ->int {return neededToSetMaxTo(v) <= k; };
+    auto lMax = makeFunctorIterator(int64_t (-1), maxFunctor);
+    auto rMax = makeFunctorIterator(int64_t(2e9 + 1), maxFunctor);
+    auto maxV = lower_bound(lMax, rMax, 1).getParameter();
+
+    cout << max(maxConverge - minConverge, maxV  - minV);
+}
+
+
+class Solver671BTest : public ProblemTest {};
+
+
+TEST_F(Solver671BTest, Example1) {
+    string input = R"(4 1
+1 1 4 2
+)";
+    string output = R"(2
+)";
+    output.pop_back();
+    setInput(input);
+    Solver671B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+TEST_F(Solver671BTest, Example2) {
+    string input = R"(3 1
+2 2 2
+)";
+    string output = R"(0
+)";
+    output.pop_back();
+    setInput(input);
+    Solver671B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+TEST_F(Solver671BTest, Example3) {
+    string input = R"(3 100
+1 2 2
+)";
+    string output = R"(1
+)";
+    output.pop_back();
+    setInput(input);
+    Solver671B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+TEST_F(Solver671BTest, Example4) {
+    string input = R"(4 7
+1 2 8 9
+)";
+    string output = R"(0
+)";
+    output.pop_back();
+    setInput(input);
+    Solver671B().run();
+    EXPECT_EQ(output, getOutput());
+}
+
+TEST_F(Solver671BTest, Example5) {
+    string input = R"(1 100
+66666
+)";
+    string output = R"(0
+)";
+    output.pop_back();
+    setInput(input);
+    Solver671B().run();
+    EXPECT_EQ(output, getOutput());
+}
