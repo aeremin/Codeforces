d6e1352c3ef2df25b70920dbcf7a13e154678ba0
Alexey Eremin <a.eremin.msu@gmail.com>
2016-09-25 14:45:00 +0300

    Fix gcc build. Use non-standard __gnu_pbds::tree only when sending problem to online judge compiler, use stub version locally. The issue is in tr1::tuple/std::tuple/googletest::tuple conflicts.

--- alexey/algo/ordered_set.h
+++ alexey/algo/ordered_set.h
@@ -1,6 +1,17 @@
 #pragma once
 
-#if (defined __GNUC__) && !(defined __clang__)
+#ifdef LOCAL_PC
+#include <set>
+
+template<typename T>
+class ordered_set : public std::set<T> {
+public:
+    typename std::set<T>::iterator find_by_order(size_t ord) { return std::next(this->begin(), ord); }
+    size_t order_of_key(const T& key) const { return std::distance(this->begin(), std::lower_bound(this->begin(), this->end(), key)); }
+};
+
+#else
+
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 
@@ -11,17 +22,4 @@ using ordered_set = __gnu_pbds::tree<T,
                          __gnu_pbds::rb_tree_tag,
                          __gnu_pbds::tree_order_statistics_node_update>;
 
-#else
-// Very dumb version (complexity is too big to be actually used). 
-// Just for compilability check.
-
-#include <set>
-
-template<typename T>
-class ordered_set : public std::set<T> {
-public:
-    typename std::set<T>::iterator find_by_order(size_t ord) { return std::next(this->begin(), ord); }
-    size_t order_of_key(const T& key) const { return std::distance(this->begin(), std::lower_bound(this->begin(), this->end(), key)); }
-};
-
-#endif
+#endif // LOCAL_PC
