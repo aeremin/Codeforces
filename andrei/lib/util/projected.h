b606739635a7cb361f39aea9dc017427702c9338
Andrei Matveiakin <a.matveyakin@gmail.com>
2018-08-03 23:51:28 +0200

    Add 'projected' predicate for std::sort and alike

---
+++ andrei/lib/util/projected.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <utility>
+
+
+template<typename Projector, typename Comparator>
+class Projected {
+public:
+  Projected(Projector projector, Comparator comparator)
+    : projector_(std::move(projector)), comparator_(std::move(comparator)) {}
+
+  template<typename T>
+  bool operator()(const T& a, const T& b) {
+    return comparator_(projector_(a), projector_(b));
+  }
+
+private:
+  Projector projector_;
+  Comparator comparator_;
+};
+
+template<typename Projector>
+class Projected<Projector, void> {
+public:
+  explicit Projected(Projector projector) : projector_(std::move(projector)) {}
+
+  template<typename T>
+  bool operator()(const T& a, const T& b) {
+    return projector_(a) < projector_(b);
+  }
+
+private:
+  Projector projector_;
+};
+
+
+template<typename Projector>
+Projected<Projector, void> projected(Projector projector) {
+  return Projected<Projector, void>(std::move(projector));
+}
+
+template<typename Projector, typename Comparator>
+Projected<Projector, Comparator> projected(Projector projector, Comparator comparator) {
+  return {std::move(projector), std::move(comparator)};
+}
---
+++ andrei/lib/util/projected_test.cpp
@@ -0,0 +1,30 @@
+#include "util/projected.h"
+
+#include "iter/view.h"
+
+#include <algorithm>
+#include <vector>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+
+using testing::ElementsAre;
+
+TEST(ProjectedTest, NoComparator) {
+  std::vector<int> v = {10, 30, 24, 90, 77, 54};
+  std::sort(v.begin(), v.end(), projected([](int a) { return a / 10; }));
+  EXPECT_THAT(v, ElementsAre(10, 24, 30, 54, 77, 90));
+}
+
+TEST(ProjectedTest, WithComparator) {
+  std::vector<int> v = {10, 30, 24, 90, 77, 54};
+  std::sort(v.begin(), v.end(), projected([](int a) { return a / 10; },
+                                          [](int a, int b) { return a > b; }));
+  EXPECT_THAT(v, ElementsAre(90, 77, 54, 30, 24, 10));
+}
+
+TEST(ProjectedTest, OtherType) {
+  std::vector<std::string> v = {"aaa", "b", "ccccc", "dd"};
+  std::sort(v.begin(), v.end(), projected([](const std::string& s) { return s.size(); }));
+  EXPECT_THAT(v, ElementsAre("b", "dd", "aaa", "ccccc"));
+}
