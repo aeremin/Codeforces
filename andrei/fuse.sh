c8d387616227ed70410cb97702095a8e1e7c3d11
Andrei Matveiakin <a.matveyakin@gmail.com>
2018-08-06 22:53:05 +0200

    Call test.sh from fuse.sh

--- andrei/fuse.sh
+++ andrei/fuse.sh
@@ -1,12 +1,15 @@
 set -e
 
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 solution_file"
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 contest problem (e.g. $0 239 a)"
     exit 1
 fi
 
+./test.sh "$@"
+problem_upper=`echo "$2" | awk '{print toupper($0)}'`
+solution_file="codeforces/$1/$problem_upper/$2.cpp"
 tmpfile=$(mktemp --suffix=.cpp)
-./build.dbg/solution_fuser --include-path lib --include-path ../common/lib --input-file $1 > "${tmpfile}"
+./build.dbg/solution_fuser --include-path lib --include-path ../common/lib --input-file "$solution_file" > "$tmpfile"
 cd /tmp
 clang++ -std=c++17 "${tmpfile}"
-cat "${tmpfile}" | xclip -sel clip
+cat "$tmpfile" | xclip -sel clip
