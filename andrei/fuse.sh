7976602752c754267029418d3f836ec077fd7dad
Andrei Matveiakin <a.matveyakin@gmail.com>
2018-08-09 00:06:59 +0200

    Add file size warning in fuse.sh

--- andrei/fuse.sh
+++ andrei/fuse.sh
@@ -9,7 +9,25 @@ fi
 problem_upper=`echo "$2" | awk '{print toupper($0)}'`
 solution_file="codeforces/$1/$problem_upper/$2.cpp"
 tmpfile=$(mktemp --suffix=.cpp)
-./build.dbg/solution_fuser --include-path lib --include-path ../common/lib --input-file "$solution_file" > "$tmpfile"
+./build.dbg/solution_fuser --include-path lib --include-path ../common/lib \
+    --input-file "$solution_file" > "$tmpfile"
+echo
+
+file_size_bytes=`du -b "$tmpfile" | cut -f1`
+file_size_kb=$(( $file_size_bytes / 1024 ))
+limit_size_bytes=65536
+limit_size_kb=$(( $limit_size_bytes / 1024 ))
+warning_threshold_bytes=$(( $limit_size_bytes * 3 / 4 ))
+if [[ $file_size_bytes -gt $limit_size_bytes ]]; then
+    echo -e "WARNING: file size is \e[1m\e[31m${file_size_kb}KB\e[0m," \
+            "which is above Codeforces limit (${limit_size_kb}KB)"
+    echo
+elif [[ $file_size_bytes -gt $warning_threshold_bytes ]]; then
+    echo -e "WARNING: file size is \e[1m\e[33m${file_size_kb}KB\e[0m," \
+            "which is close to Codeforces limit (${limit_size_kb}KB)"
+    echo
+fi
+
 cd /tmp
-clang++ -std=c++17 "${tmpfile}"
+clang++ -std=c++17 "$tmpfile"
 cat "$tmpfile" | xclip -sel clip
