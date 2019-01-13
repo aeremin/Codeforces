3bcc88dae4b0160fe75dc9aef5909ce856322558
Andrei Matveiakin <a.matveyakin@gmail.com>
2019-01-13 21:43:01 +0100

    Fix indentation

--- andrei/lib/util/projected.h
+++ andrei/lib/util/projected.h
@@ -6,40 +6,40 @@
 template<typename Projector, typename Comparator>
 class Projected {
 public:
-  Projected(Projector projector, Comparator comparator)
+    Projected(Projector projector, Comparator comparator)
     : projector_(std::move(projector)), comparator_(std::move(comparator)) {}
 
-  template<typename T>
-  bool operator()(const T& a, const T& b) {
-    return comparator_(projector_(a), projector_(b));
-  }
+    template<typename T>
+    bool operator()(const T& a, const T& b) {
+        return comparator_(projector_(a), projector_(b));
+    }
 
 private:
-  Projector projector_;
-  Comparator comparator_;
+    Projector projector_;
+    Comparator comparator_;
 };
 
 template<typename Projector>
 class Projected<Projector, void> {
 public:
-  explicit Projected(Projector projector) : projector_(std::move(projector)) {}
+    explicit Projected(Projector projector) : projector_(std::move(projector)) {}
 
-  template<typename T>
-  bool operator()(const T& a, const T& b) {
-    return projector_(a) < projector_(b);
-  }
+    template<typename T>
+    bool operator()(const T& a, const T& b) {
+        return projector_(a) < projector_(b);
+    }
 
 private:
-  Projector projector_;
+    Projector projector_;
 };
 
 
 template<typename Projector>
 Projected<Projector, void> projected(Projector projector) {
-  return Projected<Projector, void>(std::move(projector));
+    return Projected<Projector, void>(std::move(projector));
 }
 
 template<typename Projector, typename Comparator>
 Projected<Projector, Comparator> projected(Projector projector, Comparator comparator) {
-  return {std::move(projector), std::move(comparator)};
+    return {std::move(projector), std::move(comparator)};
 }
