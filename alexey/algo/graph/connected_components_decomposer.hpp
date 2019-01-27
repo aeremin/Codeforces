984d56540d6e3ea82cda920be995cfd981446fca
Alexey Eremin <a.eremin.msu@gmail.com>
2019-01-27 20:03:17 +0100

    Modernize interface of ConnectedComponentsDecomposer, add some comment

--- alexey/algo/graph/connected_components_decomposer.hpp
+++ alexey/algo/graph/connected_components_decomposer.hpp
@@ -6,12 +6,12 @@
 #include "graph/graph.h"
 #include "graph/traversal.h"
 
+// Decomposes undirected graph into connected components.
+// Run time is O(V+E).
 class ConnectedComponentsDecomposer {
   public:
-    explicit ConnectedComponentsDecomposer(const UndirectedGraph<>& g) : graph_(g) {}
-
-    void run() {
-        int nVertices = graph_.num_vertices();
+      explicit ConnectedComponentsDecomposer(const UndirectedGraph<>& g) {
+        int nVertices = g.num_vertices();
         vertexToComponent_.clear();
         vertexToComponent_.resize(nVertices);
         fill(begin(vertexToComponent_), end(vertexToComponent_), -1);
@@ -22,23 +22,20 @@ class ConnectedComponentsDecomposer {
                 continue;  // Lies in processed component
 
             components_.push_back(std::vector<int>());
-            dfs(graph_, {i}, 
-                [&](const GraphTraversalState& state, int index) {
-                    components_.back().push_back(index);
-                    vertexToComponent_[index] = iComponent;
-                    return IterationControl::Proceed;
-                });
+            dfs(g, {i}, [&](const GraphTraversalState& state, int index) {
+                components_.back().push_back(index);
+                vertexToComponent_[index] = iComponent;
+                return IterationControl::Proceed;
+            });
             iComponent++;
         }
     }
 
-    const std::vector<int>& getVertexToComponentMap() const { return vertexToComponent_; }
+    const std::vector<int>& vertext_to_component() const { return vertexToComponent_; }
 
-    const std::vector<std::vector<int>>& getComponents() const { return components_; }
+    const std::vector<std::vector<int>>& components() const { return components_; }
 
   private:
-    const UndirectedGraph<>& graph_;
-
     std::vector<int> vertexToComponent_;
     std::vector<std::vector<int>> components_;
 };
--- alexey/algo/graph/connected_components_decomposer_test.cpp
+++ alexey/algo/graph/connected_components_decomposer_test.cpp
@@ -10,12 +10,11 @@ TEST(ConnectedComponentsDecomposerTest, WorksOnSimpleCycle)
     const int nVertices = 10;
     auto g = GraphHelper::createSimpleCycleGraph(nVertices);
     ConnectedComponentsDecomposer decomposer(g);
-    decomposer.run();
 
-    for (int elt : decomposer.getVertexToComponentMap())
+    for (int elt : decomposer.vertext_to_component())
         EXPECT_EQ(0, elt);
 
-    auto components = decomposer.getComponents();
+    auto components = decomposer.components();
     EXPECT_EQ(1, components.size());
     EXPECT_THAT(components[0], UnorderedElementsAre(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
 }
@@ -33,13 +32,12 @@ TEST(ConnectedComponentsDecomposerTest, WorksOnConcreteTwoComponentsGraph)
     g.add_edge(2, 4);
 
     ConnectedComponentsDecomposer decomposer(g);
-    decomposer.run();
 
     std::vector<int> expectedComponent = { 0, 0, 1, 0, 1 };
     for (int i = 0; i < 5; ++i)
-        EXPECT_EQ(expectedComponent[i], decomposer.getVertexToComponentMap()[i]);
+        EXPECT_EQ(expectedComponent[i], decomposer.vertext_to_component()[i]);
 
-    auto components = decomposer.getComponents();
+    auto components = decomposer.components();
     EXPECT_EQ(2, components.size());
     EXPECT_THAT(components[0], UnorderedElementsAre(0, 1, 3));
     EXPECT_THAT(components[1], UnorderedElementsAre(2, 4));
