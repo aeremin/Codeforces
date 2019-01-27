29c3d2fd331a7150c0351709082781a66a1145fd
Alexey Eremin <a.eremin.msu@gmail.com>
2019-01-28 00:20:44 +0100

    Use RootedGraph instead of DepthFirstSearcher in 580C

--- alexey/Solvers/5xx/580/Solver580C.cpp
+++ alexey/Solvers/5xx/580/Solver580C.cpp
@@ -1,6 +1,8 @@
-#include "Solvers/pch.h"
+#include <Solvers/pch.h>
 #include "graph/graph.h"
 #include "algo/graph/DepthFirstSearch.hpp"
+#include "algo/graph/rooted_graph.h"
+#include "iter/range.h"
 
 // Solution for Codeforces problem http://codeforces.com/contest/580/problem/C
 class Solver580C
@@ -26,20 +28,18 @@ void Solver580C::run()
         g.add_edge(a - 1, b - 1);
     }
 
-    DepthFirstSearcher searcher(g);
+    RootedGraph rooted(g, 0);
     
     int currentCats = 0;
     vector<size_t> nCatsInARow(nVertices);
 
-    searcher.setVertexPreprocessCallback([&](int vInd)->bool
-    {
-        auto parentCatsInARow = (vInd == 0) ? 0 : nCatsInARow[searcher.getParent(vInd)];
-        nCatsInARow[vInd] = (hasCat[vInd] == 1) ? parentCatsInARow + 1 :
-            (parentCatsInARow > maxCatsAllowed ? parentCatsInARow : 0);
-        return false;
-    });
-
-    searcher.search(0);
+    for (int depth = 0; rooted.vertices_with_depth(depth).size() > 0; ++depth) {
+        for (int vInd : rooted.vertices_with_depth(depth)) {
+            auto parentCatsInARow = (vInd == 0) ? 0 : nCatsInARow[*rooted.parent(vInd)];
+            nCatsInARow[vInd] = (hasCat[vInd] == 1) ? parentCatsInARow + 1 :
+                (parentCatsInARow > maxCatsAllowed ? parentCatsInARow : 0);
+        }
+    }
 
     int canVisit = 0;
     for (size_t i = 1; i < nVertices; ++i)
